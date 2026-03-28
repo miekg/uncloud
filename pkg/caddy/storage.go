75eeea48e6b8fab3ed11b87b2ffd5b3e201dd9a2
Miek Gieben <miek@miek.nl>
2026-03-28 07:30:08 +0100

    hook it up
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- pkg/caddy/storage.go
+++ pkg/caddy/storage.go
@@ -1,52 +1,100 @@
 package caddy
 
 import (
-	"net/netip"
+	"context"
+	"time"
 
-	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/certmagic"
+	"github.com/psviderski/uncloud/internal/corrosion"
 	"github.com/psviderski/uncloud/internal/machine/store"
 )
 
-func init() {
-	caddy.RegisterModule(Corrosion{})
-}
-
-type Corrosion struct {
-	endpoint netip.AddrPort
-	s        *store.Store
-}
-
-// CaddyModule register the module in caddy. The returned ModuleInfo must have both a name and a constructor function. This method must not have
-// any side-effects.
-func (Corrosion) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID: "caddy.storage.uncloud",
-		New: func() caddy.Module {
-			return new(Corrosion)
-		},
-	}
-}
-
-// CertMagicStorage converts c to a certmagic.Storage instance.
-func (c *Corrosion) CertMagicStorage() (certmagic.Storage, error) { return c, nil }
-
-// UnmarshalCaddyfile sets up the storage module from Caddyfile tokens. Syntax:
-//
-//	uncloud <endpoint>
-func (c *Corrosion) UnmarshalCaddyfile(d *caddyfile.Dispenser) (err error) {
-	for d.Next() {
-		if !d.NextArg() {
-			return d.ArgErr()
+func (c *Corrosion) Store(ctx context.Context, key string, value []byte) error {
+	if c.s == nil {
+		if err := c.init(); err != nil {
+			return err
 		}
-		c.endpoint, err = netip.ParseAddrPort(d.Val())
 	}
-	return err
+
+	return c.s.Put(ctx, key, value)
 }
 
-var (
-	_ caddy.StorageConverter = (*Corrosion)(nil)
-	_ caddyfile.Unmarshaler  = (*Corrosion)(nil)
-	_ certmagic.Storage      = (*Corrosion)(nil)
-)
+func (c *Corrosion) Load(ctx context.Context, key string) ([]byte, error) {
+	if c.s == nil {
+		if err := c.init(); err != nil {
+			return nil, err
+		}
+	}
+
+	value := []byte{}
+	err := c.s.Get(ctx, key, &value)
+	return value, err
+}
+
+func (c *Corrosion) Delete(ctx context.Context, key string) error {
+	if c.s == nil {
+		if err := c.init(); err != nil {
+			return err
+		}
+	}
+
+	return c.s.Delete(ctx, key)
+}
+
+func (c *Corrosion) Exists(ctx context.Context, key string) bool {
+	if c.s == nil {
+		if err := c.init(); err != nil {
+			return false
+		}
+	}
+
+	_, err := c.Load(ctx, key)
+	return err == nil
+}
+
+func (c *Corrosion) Stat(ctx context.Context, key string) (certmagic.KeyInfo, error) {
+	if c.s == nil {
+		if err := c.init(); err != nil {
+			return certmagic.KeyInfo{}, err
+		}
+	}
+
+	value, err := c.Load(ctx, key)
+	if err != nil {
+		return certmagic.KeyInfo{}, err
+	}
+
+	return certmagic.KeyInfo{
+		Key:        key,
+		Modified:   time.Now().UTC(),
+		Size:       int64(len(value)),
+		IsTerminal: true,
+	}, nil
+}
+
+func (c *Corrosion) List(ctx context.Context, prefix string, recursive bool) ([]string, error) {
+	if c.s == nil {
+		if err := c.init(); err != nil {
+			return nil, err
+		}
+	}
+
+	list, err := c.s.List(ctx, prefix)
+	if err != nil {
+		return nil, err
+	}
+	// TODO(miek): handle recurse
+	return list, nil
+}
+
+func (s *Corrosion) Lock(ctx context.Context, key string) error   { return nil }
+func (c *Corrosion) Unlock(ctx context.Context, key string) error { return nil }
+
+func (c *Corrosion) init() error {
+	corro, err := corrosion.NewAPIClient(c.addr)
+	if err != nil {
+		return err
+	}
+	c.s = store.New(corro)
+	return nil
+}
